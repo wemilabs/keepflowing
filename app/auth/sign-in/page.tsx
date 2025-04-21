3fa932f0da8fce2e90f2eedd4861932899c9bdd7
meredith <dmzart921@gmail.com>
2025-04-21 11:27:34 +0200

    feat: Style sign-in page and update project configuration files

--- app/auth/sign-in/page.tsx
+++ app/auth/sign-in/page.tsx
@@ -10,7 +10,7 @@ export const metadata: Metadata = {
 
 export default function page() {
   return (
-    <div className="container flex h-screen w-screen flex-col items-center justify-center">
+    <div className="flex h-screen flex-col items-center justify-center border border-amber-600">
       <div className="absolute top-4 left-4">
         <BackButton
           variant="ghost"
@@ -18,7 +18,7 @@ export default function page() {
           className="font-semibold"
         />
       </div>
-      <div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
+      <div className="mx-auto flex flex-col justify-center space-y-6 sm:w-[350px]">
         <div className="flex flex-col space-y-2 text-center">
           <h1 className="text-2xl font-semibold tracking-tight">
             Just one step before!
--- eslint.config.mjs
+++ eslint.config.mjs
@@ -12,6 +12,7 @@ const eslintConfig = [
       "@next/next/no-page-custom-font": "off",
       "@next/next/no-img-element": "off",
     },
+    ignorePatterns: ["generated/**/*"],
   }),
 ];
 
